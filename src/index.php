86119b92e70c71e82469c78d0f0d3a7ebfb2907b
RoudePH <sgaras@gerc.ua>
2023-12-28 09:39:40 +0200

    refactor

--- src/index.php
+++ src/index.php
@@ -2,7 +2,11 @@
 
 try {
     $text = "Hello world!";
+
+//    var_dump($text);
+
     require('./pages/init-page.tpl');
 } catch (error) {
     require('./pages/error-page.tpl');
 }
+
--- src/pages/init-page.tpl
+++ src/pages/init-page.tpl
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-<h1> <?php echo $text; ?> </h1>
+<h1> <?= $text; ?> </h1>
 
 </body>
 </html>
